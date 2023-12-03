85afdf7606066611c98974076811c6d06bf3a9bb
Ayajahamad <ayazahmad2362001@gmail.com>
2023-12-03 18:42:42 +0530

    REACT-CRUD Project id Updated

--- REACT/class11-crud/src/data/user.js
+++ REACT/class11-crud/src/data/user.js
@@ -2,34 +2,42 @@ import { toast } from "react-toastify";
 // initial data
 const users = JSON.parse(localStorage.getItem('users')) || [];
 console.log("Users :",users);
-console.log("Users.Email :",users.email);
+// console.log("Users.Email :",users.email);
 
 // Register
 const registerUser = async (user) => {
     console.log('Register =', user);
 
-    const extEmail=()=>{
-        return users.email === user.email;
-    }
-    const extMobile=()=>{
-        return users.mobile === user.mobile;
-    }
+    const extEmail = users.find((item)=> item.email === user.email);
+    const extMobile = users.find((item)=> item.mobile === user.mobile);
 
     if(extEmail){
-        toast.warning(`${user.email} already Exists`)
+        toast.warning(`${user.email} is already Exists`)
     }else if(extMobile){
-        toast.warning(`${user.mobie} already Exists`)
+        toast.warning(`${user.mobile} is already Exists`)
     }else{
-        toast.success(`Hi, ${user.name} You have registered Successfully`);
-        //window.location.href="/login";
-    }
+        users.push(user);
+        console.log(users);
+        saveUsers(users)
 
-    saveUser(user)
+        toast.success(`Hi ${user.name} You have registered Successfully`);
+        window.location.href = "/login";
+    }
 };
 
 // Save data
-const saveUser=(data)=>{
-    localStorage.setItem('users',JSON.stringify(data));
+const saveUsers=(data)=>{
+    localStorage.setItem('users', JSON.stringify(data));
 }
 
-export {registerUser};
+const loginUser= async (user)=>{
+    const extUser = users.find((item)=> item.email === user.email);
+
+    if (!extUser) {
+        toast.warning(`${user.email} doesn't exists.`)
+    } else {
+        toast.success(`${user.email} exists.`)
+    }
+}
+
+export {registerUser,loginUser};

290b10cfaffcaffcdae1f6420cb478292bc7de6c
Ayajahamad <ayazahmad2362001@gmail.com>
2023-12-07 06:46:20 +0530

    REACT-CRUD App Updated with create page

--- REACT/class11-crud/src/data/task.js
+++ REACT/class11-crud/src/data/task.js
@@ -1,6 +1,43 @@
+import { toast } from "react-toastify";
+
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// console.log("TASK ARRAY = ",tasks);
+
+// RAND ID() Generator
+ const randTd = () => {
+    return Math.floor(Math.random()*1000);
+ }
+// console.log("Rand ID" , randTd());
+
+
+// Save the Task
+const saveTask = (data) => {
+    localStorage.setItem("tasks", JSON.stringify(data));
+}
+
 // Store the tasks
 const createTask = async (task) => {
-    console.log("TASK.JS = ", task);
+    // console.log("TASK.JS = ", task);
+
+    try {
+        const extTask = tasks.find((item)=>item.title === task.title);
+        if (extTask) {
+            toast.warning(`Task is already exists`)
+        } else {
+            const newTask = {
+                id: randTd(),
+                ...task,
+            };
+            tasks.push(newTask);
+            saveTask(tasks);
+            toast.success("New task created successfully");
+            window.location.href = "/";
+        }
+        }
+        catch (error) {
+        toast.error(error.message);
+    }
 }
+
 
 export {createTask};

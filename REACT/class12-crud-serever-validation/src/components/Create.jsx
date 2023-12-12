9b8b70ff51d68f589bab34190808534e3aec6c4d
Ayajahamad <ayazahmad2362001@gmail.com>
2023-12-12 18:45:57 +0530

    Crud App Added to server

--- REACT/class12-crud-serever-validation/src/components/Create.jsx
+++ REACT/class12-crud-serever-validation/src/components/Create.jsx
@@ -42,22 +42,37 @@ const Create = () => {
                   <div className="form-group my-2">
                     <label htmlFor="name">Name</label>
                     <input type="text" name="name" id="name" className='form-control' required minLength={4} maxLength={10} onChange={readValue} value={contact.name}/>
+                    {
+                      errors.name ? (<div className="alert alert-danger">{errors.name}</div>) : null
+                    }
                   </div>
                   <div className="form-group my-2">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" className='form-control' placeholder='username@domain.com' onChange={readValue} value={contact.email}/>
+                    <input type="email" name="email" id="email" className='form-control' placeholder='username@domain.com' required onChange={readValue} value={contact.email}/>
+                    {
+                      errors.email ? (<div className="alert alert-danger">{errors.email}</div>) : null
+                    }
                   </div>
                   <div className="form-group my-2">
                     <label htmlFor="image">Profile Image</label>
                     <input type="url" name="image" id="image" className='form-control' required placeholder='Enter url formate of image link' onChange={readValue} value={contact.image}/>
+                    {
+                      errors.image ? (<div className="alert alert-danger">{errors.image}</div>) : null
+                    }
                   </div>
                   <div className="form-group my-2">
                     <label htmlFor="mobile">Mobile</label>
                     <input type="number" name="mobile" id="mobile" className='form-control' required onChange={readValue} value={contact.mobile}/>
+                    {
+                      errors.mobile ? (<div className="alert alert-danger">{errors.mobile}</div>) : null
+                    }
                   </div>
                   <div className="form-group my-2">
                     <label htmlFor="address">Address</label>
                     <textarea name="address" id="address" cols="30" rows="5" className='form-control' required onChange={readValue} value={contact.address}></textarea>
+                    {
+                      errors.address ? (<div className="alert alert-danger">{errors.address}</div>) : null
+                    }
                   </div>
                   <div className="form-group my-2">
                     <input type="submit" value="Create" className='btn btn-outline-success'/>
--- REACT/class12-crud-serever-validation/src/middleware/FormValidate.js
+++ REACT/class12-crud-serever-validation/src/middleware/FormValidate.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import { omit } from "lodash";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const initState = {
     name : "",
@@ -8,7 +10,7 @@ const initState = {
     address  : ""
 };
 
-// useForm = > form validate custom hook
+// useForm => form validate custom hook
 function UserForm(){
     const [contact, setContact] = useState(initState);
     const [errors, setError] = useState({});
@@ -18,14 +20,65 @@ function UserForm(){
         setError({...errors , [prop]:msg});
     }
 
+    // Validation function
     const validate = (event,name,value) => {
         switch (name) {
             case "name":
                 if (value.length === 0) {
-                    errPrint(name,"Email field must be filled");
+                    errPrint(name,"Name field must be filled");
+                } else if(value.length < 3){
+                    errPrint(name, "Name at least 3 letters")
+                }else if(!new RegExp(/^[a-zA-Z\s]+$/).test(value)){
+                    errPrint(name,"Invalid Name")
+                }else{
+                    let newOb = omit(errors,name);
+                    setError(newOb)
                 }
                 break;
-        
+            
+            case "email": 
+                if (value.length === 0) {
+                    errPrint(name,"Email field must be filled");
+                } else if(!new RegExp(/^[a-zA-Z0-9\S]+@[a-zA-Z\S]+$/).test(value)){
+                    errPrint(name,"Invalid Email Format.")
+                }else{
+                    let newOb = omit(errors,name);
+                    setError(newOb)
+                }
+                break;
+
+            case "image":
+                if (value.length === 0) {
+                    errPrint(name,"Image field must be filled");
+                } else if(!new RegExp(/^\S+$/).test(value)){
+                    errPrint(name,"Invalid Image URL.")
+                }else{
+                    let newOb = omit(errors,name);
+                    setError(newOb)
+                }
+                break;
+
+            case "mobile":
+                if (value.length === 0) {
+                    errPrint(name,"Mobile field must be filled");
+                } else if(!new RegExp(/^[6-9]\d{9}$/).test(value)){
+                    errPrint(name,"Invalid Mobile Number.")
+                }else{
+                    let newOb = omit(errors, name);
+                    setError(newOb)
+                }
+                break;
+            case "address":
+                if (value.length === 0) {
+                    errPrint(name,"Address field must be filled");
+                } else if(!new RegExp(/^([a-zA-Z0-9/\\''(),-/#\s]{2,255})$/).test(value)){
+                    errPrint(name,"Invalid Address.")
+                }else{
+                    let newOb = omit(errors,"name");
+                    setError(newOb)
+                }
+                break;
+
             default:
                 break;
         }
@@ -41,7 +94,12 @@ function UserForm(){
     // For Submit The value
     const submitHandler = (e) => {
         e.preventDefault(); // avoide page refresh
-        console.log("New Contact = ",contact);
+        // console.log("New Contact = ",contact);
+        if(Object.keys(errors).length === 0 && Object.keys(contact).length !== 0){
+            console.log("New Contact = ",contact);
+        }else{
+            toast("Some Errors are in from or Fields are empty")
+        }
     }
 
     return {
@@ -49,7 +107,7 @@ function UserForm(){
         errors,
         readValue,
         submitHandler,
-        setContact
+        setContact,
     }
 }
 
